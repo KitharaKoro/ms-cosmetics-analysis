18e365a4e75916f722404f980ab69a3ff45a9d67
Tim <timothy.park@mail.utoronto.ca>
2018-11-08 18:07:37 -0500

    download code for all hair styles. Split joint images for males

--- chickenscratch.py
+++ chickenscratch.py
@@ -1,14 +1,17 @@
-import os
-from selenium.webdriver import Firefox
+# import os
+# from selenium.webdriver import Firefox
 import pyautogui
 import time
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import Chrome
 from scrapy.selector import Selector
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 import csv
-
+from skimage.feature import match_template
+from skimage.io import imread
+from skimage.io import imshow
+import re
 
 print("Herp Derp")
 
@@ -33,7 +36,6 @@ and they lived at the bottom of a well.</p>
 <p class="story">...</p>
 """
 
-
 soup = BeautifulSoup(html_doc, 'html.parser')
 
 print(soup.prettify())
@@ -48,7 +50,7 @@ soup2 = BeautifulSoup(page, 'html.parser')
 print(soup2.prettify())
 
 import requests
-import re
+
 
 name = list()
 classm = list()
@@ -152,29 +154,30 @@ for x in range(5):
     print(html.xpath('//tr/td/b/text()').extract()[5 * x + 2])
     print(html.xpath('//tr/td/b/text()').extract()[5 * x + 3])
     print(html.xpath('//tr/td/b/text()').extract()[5 * x + 4])
-    print(
-        re.split(r'<br>|</td>', html.xpath('//tr/comment()[contains(., "job")]/following-sibling::*[1]').extract()[x])[
-            1])
+    #print(
+    #    re.split(r'<br>|</td>', html.xpath('//tr/comment()[contains(., "job")]/following-sibling::*[1]').extract()[x])[
+    #        1])
+    print(html.xpath('//tr/comment()[contains(., "job")]/following-sibling::*[1]/text()').extract()[x])
 # not sure if this is most efficient way to write. Use junk dummy instead?
 
 
 with open('Legends.csv', 'a', newline='') as file:
     filewriter = csv.writer(file, quotechar='|', quoting=csv.QUOTE_MINIMAL)
-#    filewriter.writerow(['Rank', 'IGN', 'Fame', 'Level', 'Cards', 'Class'])
+    #    filewriter.writerow(['Rank', 'IGN', 'Fame', 'Level', 'Cards', 'Class'])
     for x in range(5):
         filewriter.writerow(list((html.xpath('//tr/td/b/text()').extract()[5 * x],
-                            html.xpath('//tr/td/b/text()').extract()[5 * x + 1],
-                            html.xpath('//tr/td/b/text()').extract()[5 * x + 2],
-                            html.xpath('//tr/td/b/text()').extract()[5 * x + 3],
-                            html.xpath('//tr/td/b/text()').extract()[5 * x + 4],
-                            re.split(r'<br>|</td>',
-                                     html.xpath('//tr/comment()[contains(., "job")]/following-sibling::*[1]').extract()[
-                                         x])[1])))
+                                  html.xpath('//tr/td/b/text()').extract()[5 * x + 1],
+                                  html.xpath('//tr/td/b/text()').extract()[5 * x + 2],
+                                  html.xpath('//tr/td/b/text()').extract()[5 * x + 3],
+                                  html.xpath('//tr/td/b/text()').extract()[5 * x + 4],
+                                  html.xpath('//tr/comment()[contains(., "job")]/following-sibling::*[1]/text()')
+                                  .extract()[x]
+                                  )))
 
 browser.find_element_by_xpath('//li/a[contains(.,"Next")]').click()
-#browser.get('https://maplelegends.com/ranking/monsterbook?page=2&search=')
+# browser.get('https://maplelegends.com/ranking/monsterbook?page=2&search=')
 html = Selector(text=browser.execute_script("return document.documentElement.outerHTML;"))
-#html = Selector(text=html)
+# html = Selector(text=html)
 
 # downloading images
 browser.get('https://maplelegends.com/avatar/create.php?name=Nell')
@@ -193,9 +196,6 @@ action.perform()
 
 ActionChains(browser).context_click(elem).perform()
 
-
-
-
 # FIRST TEST
 
 # ActionChains(browser).context_click(elem).perform()
@@ -212,7 +212,6 @@ ActionChains(browser).context_click(elem).perform()
 
 # pyautogui.typewrite(['backspace', junk ], interval=1)      # ok, so typewrite works if 1 character at a time
 # junk = "Nell"
-
 
 
 # THE FUN FOR 5 IMAGES AT ONCE STARTS HERE!!!
@@ -268,20 +267,16 @@ browser = Firefox('/Users/Tim/PyCharmProjects/learning/geckodriver')
 browser = Firefox()
 browser.get('https://maplelegends.com/ranking/monsterbook?page=1&search=')
 
-
-
 print(os.getcwd())
 
-
-#image processing / classification
+# image processing / classification
 
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib.image as mpimg
 from skimage.color import rgb2grey
-from skimage.io import imread
-from skimage.io import imshow
+
 
 # img_rgb = mpimg.imread('/Users/Tim/PyCharmProjects/learning/Legends_Avatars/Nell.png')
 img = imread('/Users/Tim/PyCharmProjects/learning/Legends_Avatars/Nell.png', as_gray=True)
@@ -297,13 +292,257 @@ imshow(img)
 
 
 # OK, SO NOW TEST RUN FOR MATCHING 2 HAIRSTYLES AGAINST NELL
+img = imread('/Users/Tim/PyCharmProjects/learning/Legends_Avatars/Nell.png', as_gray=True)
 
 matcher = imread('/Users/Tim/PyCharmProjects/learning/Legends_Avatars/soprano2.png', as_gray=True)
 imshow(matcher)
-
-from skimage.feature import match_template
-
 result = match_template(img, matcher)
+imshow(result)
 
 matcher2 = imread('/Users/Tim/PyCharmProjects/learning/Legends_Avatars/cute wave hair2.png', as_gray=True)
 result2 = match_template(img, matcher2)
+imshow(result2)
+
+matcher3 = imread('/Users/Tim/PyCharmProjects/learning/Legends_Avatars/uglyfairy2.png', as_gray=True)
+result3 = match_template(img, matcher3)
+imshow(result3)
+
+matcher4 = imread('/Users/Tim/PyCharmProjects/learning/Legends_Avatars/sad2.png', as_gray=True)
+result4 = match_template(img, matcher4)
+imshow(result4)
+
+# so, looks like eyewear really throws off match_template... Maybe should do a quick ML to classify eyewear/no eyewear
+# possible later extension!
+
+# For now, just focus on getting all of the hairs and eyes
+
+browser = Chrome('/Users/Tim/PyCharmProjects/learning/chromedriver')
+browser.get('https://forum.maplelegends.com/index.php?threads/guide-beauty-parlor-hair-face-skin.7651/')
+html = Selector(text=browser.execute_script("return document.documentElement.outerHTML;"))
+# face_url = browser.find_elements_by_xpath('//tr/td/img[contains(@class, "avatar")]')
+
+hair_url = html.xpath('//tr/td/img/@src').extract()     # gets imgur urls
+html.xpath('//tr/td/img/preceding::*[1]/text()').extract()       # gets hair / eye names
+
+browser.get(hair_url[400])
+html.xpath('//button/span/span/text()').extract_first()
+
+# Hair Sublists
+male_hair_url = html.xpath('//button/span/span[contains(., "Male Hair")]/following::*[1]//tr/td/img/@src').extract()
+female_hair_url = html.xpath('//button/span/span[contains(., "Female Hair")]/following::*[1]//tr/td/img/@src').extract()
+
+male_hair_name = html.xpath('//button/span/span[contains(., "Male Hair")]/following::*[1]//tr/td/img/preceding::*[1]/text()').extract()
+female_hair_name = html.xpath('//button/span/span[contains(., "Female Hair")]/following::*[1]//tr/td/img/preceding::*[1]/text()').extract()
+
+
+with open('Male Hair URL.txt', 'a', newline='') as file:
+    for x in range(len(male_hair_url)):
+        file.write(male_hair_url[x] + '\n')
+
+with open('Male Hair Name.txt', 'a', newline='') as file:
+    for x in range(len(male_hair_name)):
+        file.write(male_hair_name[x] + '\n')
+
+with open('Female Hair URL.txt', 'a', newline='') as file:
+    for x in range(len(female_hair_url)):
+        file.write(female_hair_url[x] + '\n')
+
+with open('Female Hair Name.txt', 'a', newline='') as file:
+    for x in range(len(female_hair_name)):
+        file.write(female_hair_name[x] + '\n')
+
+
+# now to get the hair images and snip just the black-hair copy
+
+# test run on first male hair
+# with open('Male Hair URL.txt', 'a', newline='') as file:
+#    male_hair_url = file.readlines()
+# file = open('/Users/Tim/PycharmProjects/learning/Male Hair URL.txt', 'r')
+# re.split('\n', file.readline())
+# file.close()
+# browser.get(male_hair_url[0])
+
+
+def return_line(line_number, file_name):
+    with open(file_name, 'r') as file:
+        for x in range(line_number-1):
+            file.readline()
+        return re.split('\n', file.readline())[0]
+
+
+def beforeBlank(line):
+    return re.split(' ', line)[0]
+
+
+def beforeSlash(line):
+    return re.split('/', line)[0]
+
+
+# return_line(3, '/Users/Tim/PycharmProjects/learning/Male Hair URL.txt')    # Yay it works
+
+browser.get(return_line(1, '/Users/Tim/PycharmProjects/learning/Male Hair URL.txt'))
+# browser.find_element_by_xpath("//img").click()
+# ActionChains(browser).context_click(pics[0]).perform()
+# pyautogui.typewrite(['down', 'down', 'enter'], interval=.5)
+pyautogui.moveTo(500, 60)
+pyautogui.click(interval=1)
+time.sleep(2)
+# pyautogui.press(['command', 's'])
+pyautogui.keyDown('command')
+pyautogui.keyDown('s')
+# pyautogui.keyUp('command', 's')
+pyautogui.keyUp('command')
+pyautogui.keyUp('s')
+time.sleep(2)
+# pyautogui.typewrite(['backspace'], interval=.5)
+pyautogui.typewrite(return_line(1, '/Users/Tim/PycharmProjects/learning/Male Hair Name.txt'), interval=.5)
+pyautogui.typewrite(['enter'])
+
+
+# okay, now the same for all male hair
+# should find dynamic method to find the 141...
+
+for x in range(141):
+    browser.get(return_line(x+1, '/Users/Tim/PycharmProjects/learning/Male Hair URL.txt'))
+    time.sleep(2)
+    pyautogui.moveTo(500, 60)
+    pyautogui.click(interval=1)
+    time.sleep(2)
+    pyautogui.keyDown('command')
+    pyautogui.keyDown('s')
+    pyautogui.keyUp('command')
+    pyautogui.keyUp('s')
+    time.sleep(2)
+    pyautogui.typewrite(return_line(x+1, '/Users/Tim/PycharmProjects/learning/Male Hair Name.txt'), interval=.5)
+    pyautogui.typewrite(['enter'])
+    time.sleep(5)
+
+# now to snip just the black hair
+
+# pyautogui.moveTo(40, 150)
+pyautogui.click(40, 150)
+time.sleep(1)
+pyautogui.keyDown('command')
+pyautogui.keyDown('w')
+pyautogui.keyUp('command')
+pyautogui.keyUp('w')
+time.sleep(2)
+pyautogui.keyDown('command')
+pyautogui.keyDown('o')
+pyautogui.keyUp('command')
+pyautogui.keyUp('o')
+time.sleep(5)
+# pyautogui.typewrite(beforeBlank(return_line(30, '/Users/Tim/PycharmProjects/learning/Male Hair Name.txt')), interval=.2)
+# pyautogui.typewrite(return_line(4, '/Users/Tim/PycharmProjects/learning/Male Hair Name.txt'), interval=.8)
+pyautogui.typewrite(beforeSlash(return_line(137, '/Users/Tim/PycharmProjects/learning/Male Hair Name.txt')), interval=.2)
+
+pyautogui.typewrite(['enter'])
+time.sleep(2)
+
+pyautogui.moveTo(40, 150)
+pyautogui.mouseDown(button='left')
+# pyautogui.moveTo(100, 250)
+# pyautogui.mouseUp(button='left', x=100, y=250, duration=)
+pyautogui.dragTo(100, 250, 1, button='left')
+pyautogui.mouseUp(button='left')
+
+time.sleep(1)
+pyautogui.keyDown('command')
+pyautogui.keyDown('c')
+pyautogui.keyUp('command')
+pyautogui.keyUp('c')
+time.sleep(1)
+pyautogui.keyDown('command')
+pyautogui.keyDown('n')
+pyautogui.keyUp('command')
+pyautogui.keyUp('n')
+time.sleep(1)
+pyautogui.keyDown('command')
+pyautogui.keyDown('s')
+pyautogui.keyUp('command')
+pyautogui.keyUp('s')
+time.sleep(2)
+pyautogui.typewrite(return_line(137, '/Users/Tim/PycharmProjects/learning/Male Hair Name.txt')+'s', interval=.2)
+pyautogui.typewrite(['enter'])
+time.sleep(2)
+pyautogui.keyDown('command')
+pyautogui.keyDown('w')
+pyautogui.keyUp('command')
+pyautogui.keyUp('w')
+time.sleep(2)
+pyautogui.keyDown('command')
+pyautogui.keyDown('w')
+pyautogui.keyUp('command')
+pyautogui.keyUp('w')
+time.sleep(2)
+
+# file name search breaks if you hit spacebar and slash together!!!
+
+# now to run for all male hair
+pyautogui.click(40, 150)
+for x in range(141):
+    time.sleep(5)
+    pyautogui.keyDown('command')
+    pyautogui.keyDown('w')
+    time.sleep(.5)
+    pyautogui.keyUp('command')
+    pyautogui.keyUp('w')
+    time.sleep(5)
+    pyautogui.keyDown('command')
+    pyautogui.keyDown('o')
+    time.sleep(.5)
+    pyautogui.keyUp('command')
+    pyautogui.keyUp('o')
+    time.sleep(5)
+    pyautogui.typewrite(beforeBlank(return_line(x+1, '/Users/Tim/PycharmProjects/learning/Male Hair Name.txt')),
+                        interval=.5)
+    pyautogui.typewrite(['enter'])
+    time.sleep(5)
+
+    pyautogui.moveTo(40, 150)
+    pyautogui.mouseDown(button='left')
+    pyautogui.dragTo(100, 250, 1, button='left')
+    pyautogui.mouseUp(button='left')
+    time.sleep(5)
+
+    pyautogui.keyDown('command')
+    pyautogui.keyDown('c')
+    time.sleep(.5)
+    pyautogui.keyUp('command')
+    pyautogui.keyUp('c')
+    time.sleep(5)
+    pyautogui.keyDown('command')
+    pyautogui.keyDown('n')
+    time.sleep(.5)
+    pyautogui.keyUp('command')
+    pyautogui.keyUp('n')
+    time.sleep(5)
+    pyautogui.keyDown('command')
+    pyautogui.keyDown('s')
+    time.sleep(.5)
+    pyautogui.keyUp('command')
+    pyautogui.keyUp('s')
+    time.sleep(5)
+    pyautogui.typewrite(return_line(x+1, '/Users/Tim/PycharmProjects/learning/Male Hair Name.txt') + 's', interval=.2)
+    pyautogui.typewrite(['enter'])
+    time.sleep(5)
+    pyautogui.keyDown('command')
+    pyautogui.keyDown('w')
+    time.sleep(.5)
+    pyautogui.keyUp('command')
+    pyautogui.keyUp('w')
+    time.sleep(5)
+    pyautogui.keyDown('command')
+    pyautogui.keyDown('w')
+    time.sleep(.5)
+    pyautogui.keyUp('command')
+    pyautogui.keyUp('w')
+    time.sleep(5)
+
+import subprocess
+# FileName = return_line(6, '/Users/Tim/PycharmProjects/learning/Male Hair Name.txt')
+subprocess.call(['open', '/Users/Tim/PycharmProjects/learning/Legends_Avatars/Male_Hair/' +
+                 return_line(6, '/Users/Tim/PycharmProjects/learning/Male Hair Name.txt') + '.png'])
+
+# a much more generalized alternative! Warning that this can be ugly with .txt files in MacOS
+
